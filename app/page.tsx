5f937cadf1a0986e3566373e9cf013b8b6ba8318
romiardanaputra <romiardana21@gmail.com>
2024-07-05 20:07:43 +0800

    chore(assets): Modify pages and default asset by next

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <section className='py-24'>
+        <div className='container'>
+          <div className='text-3xl font-bold'>Next TS Started Template</div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default page
