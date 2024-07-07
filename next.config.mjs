771e87c7ec767892125da1f9180004d8ca4cb2b8
romiardanaputra <romiardana21@gmail.com>
2024-07-07 13:32:55 +0800

    refactor(boiler plate): Add shadcn ui, modify eslint rule

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/melancong/**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,76 @@
+/* eslint-disable quotes */
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}'
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-        md: '1.5rem',
-        lg: '2rem'
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
+    extend: {
+      colors: {
+        green: {
+          500: '#24AE7C',
+          600: '#0D2A1F'
+        },
+        blue: {
+          500: '#79B5EC',
+          600: '#152432'
+        },
+        red: {
+          500: '#F37877',
+          600: '#3E1716',
+          700: '#F24E43'
+        },
+        light: {
+          200: '#E8E9E9'
+        },
+        dark: {
+          200: '#0D0F10',
+          300: '#131619',
+          400: '#1A1D21',
+          500: '#363A3D',
+          600: '#76828D',
+          700: '#ABB8C4'
+        }
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans]
+      },
+
+      backgroundImage: {
+        // any background image you want to use
+        bgLanding: "url('./assets/images/bg-landing.webp')"
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     }
   },
-  plugins: []
-}
+  plugins: [require('tailwindcss-animate')]
+} satisfies Config
+
 export default config
