9a6c7620cb8c2490b4bbb0fa5894ea5312976232
romiardanaputra <romiardana21@gmail.com>
2024-07-07 13:42:41 +0800

    feat(landing): Add landing page [mobile]

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+// import DarkModeButton from '@/components/darkmode/DarkModeToggle'
+import Image from 'next/image'
 
+const imgLanding =
+  'https://ik.imagekit.io/melancong/melancong/landing/bg-landing.webp?updatedAt=1720282428544'
 const page = () => (
   <>
-    <section className='py-24'>
-      <div className='container'>
-        <div className='text-3xl font-bold'>
-          Next TS Started Template Melancong
+    <div className='h-screen max-h-screen'>
+      <section>
+        <div className='absolute size-full bg-blend-darken'>
+          <Image
+            src={imgLanding}
+            alt='melancong-landing'
+            width={1920}
+            height={400}
+            objectFit='cover'
+            objectPosition='center'
+            className='size-full object-cover object-center opacity-80'
+            priority
+          />
         </div>
-      </div>
-    </section>
+        <div className='heading absolute flex size-full flex-col justify-between px-6 py-12 text-7xl font-black text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+          <div>
+            <span>World Of</span>
+            <span className='font-outline-2 block text-transparent'>
+              Paradise,
+            </span>
+            Bali
+          </div>
+
+          <div className='grid grid-cols-1 gap-4'>
+            <Button
+              className='bg-dark-300 p-6 py-8 font-black text-white'
+              variant='secondary'
+            >
+              EXPLORE MORE
+            </Button>
+
+            <span className='text-center text-sm text-white'>
+              Don&apos;t have an account? <a href='/login'>Login</a>
+            </span>
+          </div>
+        </div>
+      </section>
+    </div>
   </>
 )
 
