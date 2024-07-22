57a855b52eae10e100c4d86b7980c20fed18909e
romiardanaputra <romiardana21@gmail.com>
2024-07-22 19:28:33 +0800

    feat(components): Add button and card component from aceternity ui

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,80 +1,58 @@
-/* eslint-disable react/prop-types*/
-/* eslint-disable jsx-a11y/heading-has-content*/
-import * as React from 'react'
-import { cn } from '@/lib/utils'
+import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      'rounded-lg border border-slate-200 bg-white text-slate-950 shadow-sm dark:border-slate-50',
-      className
-    )}
-    {...props}
-  />
-))
-Card.displayName = 'Card'
+interface ImageCardProps {
+  title: string
+  img: string
+  rating: string
+  description: string
+  location: string
+}
 
-const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn('flex flex-col space-y-1.5 p-6', className)}
-    {...props}
-  />
-))
-CardHeader.displayName = 'CardHeader'
+const CardComponent: NextPage<ImageCardProps> = ({
+  title,
+  img,
+  rating,
+  description,
+  location
+}) => {
+  return (
+    <>
+      <div className='max-w-sm transform rounded-[22px] bg-white shadow-xl transition duration-400 hover:-translate-y-2 dark:bg-zinc-900'>
+        <div className='h-[200px] overflow-hidden rounded-lg p-0'>
+          <Image
+            src={img}
+            alt={title}
+            height='400'
+            width='400'
+            className='object-cover object-center'
+            loading='lazy'
+          />
+        </div>
+        <div className='p-5'>
+          <p className='mb-2 mt-4 text-base font-bold text-neutral-900 dark:text-neutral-200 sm:text-xl'>
+            {title}
+          </p>
+          <p className='mb-2 text-sm font-medium'>{location}</p>
+          <p className='text-sm text-neutral-600 dark:text-neutral-400'>
+            {description}
+          </p>
+          <div className='mt-4 flex items-center justify-between gap-4'>
+            <Link href='/login' className='text-sm font-medium'>
+              Read More...
+            </Link>
+            <button className='flex items-center space-x-1 rounded-full bg-black py-1 pl-4 pr-1 text-xs font-bold text-white dark:bg-zinc-800'>
+              <p>Rating </p>
+              <p className='rounded-full bg-zinc-700 px-2 py-0 text-[0.6rem] text-white'>
+                {rating}
+              </p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
-const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      'text-2xl font-semibold leading-none tracking-tight',
-      className
-    )}
-    {...props}
-  />
-))
-CardTitle.displayName = 'CardTitle'
-
-const CardDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn('text-sm text-slate-500 dark:text-slate-400', className)}
-    {...props}
-  />
-))
-CardDescription.displayName = 'CardDescription'
-
-const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('pt-0', className)} {...props} />
-))
-CardContent.displayName = 'CardContent'
-
-const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn('flex items-center p-6 pt-0', className)}
-    {...props}
-  />
-))
-CardFooter.displayName = 'CardFooter'
-
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export default CardComponent
