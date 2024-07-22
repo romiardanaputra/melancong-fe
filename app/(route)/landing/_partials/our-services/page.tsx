6c833097c8326054e55b5280992f525e890431c2
romiardanaputra <romiardana21@gmail.com>
2024-07-22 19:15:01 +0800

    feat(our service): Add our service page and turn it as partial of landing

---
+++ app/(route)/landing/_partials/our-services/page.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next'
+import { HoverEffect } from '@/components/ui/CardHoverEffect'
+import { services } from '@/data/index'
+
+interface Props {}
+
+const OurService: NextPage<Props> = () => {
+  return (
+    <>
+      <div className='container pt-20 xl:-mt-40'>
+        <h1 className='pb-12 text-center text-4xl font-medium'>Our Services</h1>
+        <div className='mx-auto'>
+          <HoverEffect items={services} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default OurService
