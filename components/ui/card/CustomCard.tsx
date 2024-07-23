99b6b2546c1dcfdaa6d7f94a106cad7ca66ccaa6
romiardanaputra <romiardana21@gmail.com>
2024-07-23 12:51:52 +0800

    refactor(components): Move and group each components into ui's folder

---
+++ components/ui/card/CustomCard.tsx
@@ -0,0 +1,91 @@
+/* eslint-disable quotes */
+import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { FaStar } from 'react-icons/fa'
+
+interface ImageCardProps {
+  title: string
+  img: string
+  rating: string
+  description: string
+  location: string
+  clickToDetail?: () => void
+  onKeyPress?: (event: React.KeyboardEvent<HTMLDivElement>) => void
+  isSaveAvailable: boolean
+  handleToggleSave?: () => void
+  isSaved?: boolean
+}
+
+const CustomCard: NextPage<ImageCardProps> = ({
+  title,
+  img,
+  rating,
+  description,
+  location,
+  clickToDetail,
+  onKeyPress,
+  isSaveAvailable,
+  handleToggleSave,
+  isSaved
+}) => {
+  return (
+    <>
+      <div
+        className='max-w-sm transform rounded-[22px] bg-white shadow-xl transition duration-400 hover:-translate-y-2 dark:bg-zinc-900'
+        role='button'
+        tabIndex={0}
+        onClick={clickToDetail}
+        onKeyPress={onKeyPress}
+      >
+        <div className='relative h-[200px] overflow-hidden rounded-lg p-0'>
+          <Image
+            src={img}
+            alt={title}
+            height='400'
+            width='400'
+            className='object-cover object-center'
+            loading='lazy'
+          />
+          {isSaveAvailable && (
+            <button
+              type='button'
+              onClick={e => {
+                e.stopPropagation()
+                if (handleToggleSave) {
+                  handleToggleSave()
+                }
+              }}
+              className={`absolute right-0 top-0 p-4 ${isSaved ? 'bg-cyan-500' : 'bg-white/80'}`}
+            >
+              <FaStar className='text-neutral-900' />
+            </button>
+          )}
+        </div>
+        <div className='p-5'>
+          <p className='mb-2 mt-4 text-base font-bold text-neutral-900 dark:text-neutral-200 sm:text-xl'>
+            {title}
+          </p>
+          <p className='mb-2 text-sm font-medium'>{location}</p>
+          <p className='text-sm text-neutral-600 dark:text-neutral-400'>
+            {description}
+          </p>
+          <div className='mt-4 flex items-center justify-between gap-4'>
+            <Link href='/login' className='text-sm font-medium'>
+              Read More...
+            </Link>
+            <button className='flex items-center space-x-1 rounded-full bg-black py-1 pl-4 pr-1 text-xs font-bold text-white dark:bg-zinc-800'>
+              <p>Rating </p>
+              <p className='rounded-full bg-zinc-700 px-2 py-0 text-[0.6rem] text-white'>
+                {rating}
+              </p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CustomCard
---
+++ components/ui/footer/Footer.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next'
+import Link from 'next/link'
+
+interface Props {}
+
+const Footer: NextPage<Props> = () => {
+  return (
+    <>
+      <div className='pt-20'>
+        <footer className='bg-neutral-900 text-neutral-100 dark:bg-gray-900'>
+          <div className='mx-auto w-full max-w-screen-xl p-4 md:py-8'>
+            <span className='block text-sm text-neutral-100 sm:text-center'>
+              © 2024{' '}
+              <Link href='#' className='hover:underline'>
+                Melancong
+              </Link>
+              . All Rights Reserved.
+            </span>
+          </div>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default Footer
