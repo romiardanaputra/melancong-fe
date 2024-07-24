12714a56012cecf041d672931d37f60006f1800a
romiardanaputra <romiardana21@gmail.com>
2024-07-24 09:01:50 +0800

    refactor(home): Add sidebar to home route and dashboard partial

--- app/(route)/home/page.tsx
+++ app/(route)/home/page.tsx
@@ -2,130 +2,130 @@
 /* eslint-disable no-shadow */
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import React, { useState, FormEvent } from 'react'
+import React, { useState } from 'react'
 import { NextPage } from 'next'
-import { useRouter } from 'next/navigation'
-import { FiSearch, FiFilter } from 'react-icons/fi'
+import { FiFilter } from 'react-icons/fi'
 import withAuth from '@/app/withAuth'
-import CustomCard from '@/components/ui/card/CustomCard'
-import SideBar from '@/components/ui/navigation/SideBar'
 import useDestinations from '@/app/hooks/useDestinations'
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarLink
+} from '@/components/ui/navigation/SideBarAnimate'
+import Image from 'next/image'
+import LogoIcon from './_partials/LogoIcon'
+import Logo from './_partials/Logo'
+import { destinationLocations, destinationTypes, links } from '@/data'
+import { cn } from '@/lib/utils'
+import DashboardUser from './_partials/pages/dashboard/page'
 
 interface Props {}
 const Home: NextPage<Props> = () => {
   const [searchQuery, setSearchQuery] = useState<string>('')
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false)
   const [locationFilter, setLocationFilter] = useState<string>('')
   const [destinationTypeFilter, setDestinationTypeFilter] = useState<string>('')
-  const {
-    destinations,
-    savedDestinations,
-    error,
-    loading,
-    fetchDestinations,
-    handleToggleSave
-  } = useDestinations()
-  const router = useRouter()
-
-  const handleSearch = (event: FormEvent) => {
-    event.preventDefault()
-    fetchDestinations(searchQuery, locationFilter, destinationTypeFilter)
-  }
-
-  const handleCardClick = (id: string) => {
-    router.push(`/destinations/${id}`)
-  }
-
-  const handleKeyPress = (
-    event: React.KeyboardEvent<HTMLDivElement>,
-    id: string
-  ) => {
-    if (event.key === 'Enter') {
-      handleCardClick(id)
-    }
-  }
+  const { fetchDestinations } = useDestinations()
 
   const handleFilterSubmit = () => {
+    alert(destinationTypeFilter)
     fetchDestinations(searchQuery, locationFilter, destinationTypeFilter)
-    setIsSidebarOpen(false)
   }
 
   const handleFilterReset = () => {
+    setSearchQuery('')
     setLocationFilter('')
     setDestinationTypeFilter('')
-    fetchDestinations(searchQuery)
-    setIsSidebarOpen(false)
+    fetchDestinations()
   }
+  const [open, setOpen] = useState(false)
 
   return (
     <>
-      <div className='container p-5'>
-        <form
-          className='mb-5 flex items-center justify-between'
-          onSubmit={handleSearch}
-        >
-          <div className='flex flex-1 items-center rounded-full bg-gray-100 p-2'>
-            <FiSearch className='mr-2' />
-            <input
-              type='text'
-              value={searchQuery}
-              onChange={e => setSearchQuery(e.target.value)}
-              placeholder='Search'
-              className='flex-1 bg-transparent outline-none'
-            />
-          </div>
-          <button
-            className='ml-2 cursor-pointer rounded-full bg-gray-100 p-2 transition duration-300 ease-in-out hover:bg-blue-100 active:bg-blue-200'
-            onClick={() => setIsSidebarOpen(true)}
-            aria-label='Open Sidebar'
-          >
-            <FiFilter />
-          </button>
-        </form>
-        <h1 className='text-lg font-bold'>Recommendation</h1>
-        {loading && (
-          <div className='flex h-screen items-center justify-center'>
-            <div className='h-10 w-10 animate-spin rounded-full border-4 border-t-4 border-gray-200 border-t-blue-500'></div>
-          </div>
+      <div
+        className={cn(
+          'mx-auto flex w-full flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800 md:flex-row',
+          'min-h-dvh' // for your use case, use `h-screen` instead of `h-[60vh]`
         )}
-        {error && <p className='text-red-500'>{error}</p>}
-        {!loading && destinations.length === 0 && (
-          <p className='text-center text-gray-500'>No destinations found</p>
-        )}
-        <div className='grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {destinations.map(destination => (
-            <CustomCard
-              key={destination.id}
-              title={destination.name}
-              img={
-                'https://firebasestorage.googleapis.com/v0/b/melanc0ng.appspot.com/o/image%2F3.jpg?alt=media&token=4b1bc0e1-5261-4c7d-a8a3-22a509fa5e09'
-              }
-              rating={destination.rating}
-              location={destination.regency}
-              description={
-                'lorem ipsum dolor sit amet concepture alet con dolor'
-              }
-              clickToDetail={() => handleCardClick(destination.id)}
-              onKeyPress={e => handleKeyPress(e, destination.id)}
-              isSaveAvailable={true}
-              handleToggleSave={() => handleToggleSave(destination.id)}
-              isSaved={savedDestinations.includes(destination.id)}
-            />
-          ))}
-        </div>
-
-        {/* sidebar component start */}
-        <SideBar
-          isSidebarOpen={isSidebarOpen}
-          setIsSidebarOpen={setIsSidebarOpen}
-          locationFilter={locationFilter}
-          setLocationFilter={setLocationFilter}
-          destinationTypeFilter={destinationTypeFilter}
-          setDestinationTypeFilter={setDestinationTypeFilter}
-          handleFilterSubmit={handleFilterSubmit}
-          handleFilterReset={handleFilterReset}
-        />
-        {/* sidebar component end */}
+      >
+        <Sidebar open={open} setOpen={setOpen} animate={false}>
+          <SidebarBody className='justify-between gap-10'>
+            <div className='flex flex-1 flex-col overflow-y-auto'>
+              {open ? <Logo /> : <LogoIcon />}
+              <div className='mt-8 flex flex-col gap-2'>
+                {links.map((link, idx) => (
+                  <SidebarLink key={idx} link={link} />
+                ))}
+                <SidebarLink
+                  link={{ label: 'filter', href: '#', icon: <FiFilter /> }}
+                />
+                <div className='mt-2'>
+                  <h3 className='mb-4 ml-2 text-sm font-semibold'>Locations</h3>
+                  <div className='flex flex-wrap gap-2'>
+                    {destinationLocations.map(location => (
+                      <button
+                        key={location}
+                        onClick={() => setLocationFilter(location)}
+                        className={`rounded-full px-3 py-1 text-sm text-neutral-700 ${locationFilter === location ? 'bg-blue-200' : 'bg-gray-200'}`}
+                      >
+                        {location}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+                <div className='mt-4'>
+                  <h3 className='mb-4 ml-2 text-sm font-semibold'>
+                    Destinations
+                  </h3>
+                  <div className='flex flex-wrap gap-2'>
+                    {destinationTypes.map(destination => (
+                      <button
+                        key={destination}
+                        onClick={() => setDestinationTypeFilter(destination)}
+                        className={`rounded-full px-3 py-1 text-sm text-neutral-700 ${destinationTypeFilter === destination ? 'bg-blue-200' : 'bg-gray-200'}`}
+                      >
+                        {destination}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <div className='mt-6 flex gap-2'>
+                    <button
+                      className='rounded-full bg-black px-6 py-2 text-sm text-neutral-200'
+                      onClick={handleFilterSubmit}
+                    >
+                      Submit
+                    </button>
+                    <button
+                      className='rounded-full border border-neutral-900 px-6 py-2 text-sm'
+                      onClick={handleFilterReset}
+                    >
+                      Reset
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <SidebarLink
+                link={{
+                  label: 'Manu Arora',
+                  href: '#',
+                  icon: (
+                    <Image
+                      src='https://assets.aceternity.com/manu.png'
+                      className='h-7 w-7 flex-shrink-0 rounded-full'
+                      width={50}
+                      height={50}
+                      alt='Avatar'
+                    />
+                  )
+                }}
+              />
+            </div>
+          </SidebarBody>
+        </Sidebar>
+        <DashboardUser />
       </div>
     </>
   )
