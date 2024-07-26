9afeb54d55a9407949d50ed8c7b2ce531831cf07
romiardanaputra <romiardana21@gmail.com>
2024-07-26 15:21:20 +0800

    refactor(home): Add filtered destinations by rating

--- app/(route)/landing/_partials/recomendation/page.tsx
+++ app/(route)/landing/_partials/recomendation/page.tsx
@@ -1,9 +1,11 @@
+'use client'
 import { NextPage } from 'next'
 import CustomCard from '@/components/ui/card/CustomCard'
-import { imageCards } from '@/data/index'
+import useDestinations from '@/app/hooks/useDestinations'
 interface Props {}
 
 const RecommendationSection: NextPage<Props> = () => {
+  const { destinations } = useDestinations()
   return (
     <>
       <div className='container pt-20'>
@@ -11,17 +13,20 @@ const RecommendationSection: NextPage<Props> = () => {
           What We Reccomends <br /> For You
         </h1>
         <div className='grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {imageCards.map(item => (
-            <CustomCard
-              key={item.id}
-              title={item.title}
-              img={item.img}
-              rating={item.rating}
-              location={item.location}
-              description={item.description}
-              isSaveAvailable={false}
-            />
-          ))}
+          {destinations
+            .filter(item => parseFloat(item.rating) > 4.8)
+            .slice(0, 8)
+            .map(item => (
+              <CustomCard
+                key={item.id}
+                title={item.name}
+                img={item.imageLink}
+                rating={parseFloat(item.rating)}
+                location={item.regency}
+                description={`${item.information.slice(0, 70)} ...`}
+                isSaveAvailable={false}
+              />
+            ))}
         </div>
       </div>
     </>
