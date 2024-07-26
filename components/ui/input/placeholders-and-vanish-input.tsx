f9dddf78e2f7bc0dbd66602470d3c97db4c3f708
romiardanaputra <romiardana21@gmail.com>
2024-07-26 14:40:33 +0800

    style(input search): Make the width full

--- components/ui/input/placeholders-and-vanish-input.tsx
+++ components/ui/input/placeholders-and-vanish-input.tsx
@@ -186,7 +186,7 @@ export function PlaceholdersAndVanishInput({
   return (
     <form
       className={cn(
-        'relative mx-auto h-12 w-full max-w-xl overflow-hidden rounded-full bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200 dark:bg-zinc-800',
+        'relative mx-auto h-12 w-full overflow-hidden rounded-full bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200 dark:bg-zinc-800',
         value && 'bg-gray-50'
       )}
       onSubmit={handleSubmit}
