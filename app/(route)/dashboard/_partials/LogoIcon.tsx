120ed51e34171f5371f21edc3e89417b717ebd05
romiardanaputra <romiardana21@gmail.com>
2024-07-31 18:53:43 +0800

    chore(Logo): Modify style and fix accessibility on Link component

--- app/(route)/dashboard/_partials/LogoIcon.tsx
+++ app/(route)/dashboard/_partials/LogoIcon.tsx
@@ -8,9 +8,10 @@ const LogoIcon: NextPage<Props> = () => {
     <>
       <Link
         href='#'
+        aria-label='melancong small logo'
         className='relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black'
       >
-        <div className='h-5 w-6 flex-shrink-0 rounded-bl-sm rounded-br-lg rounded-tl-lg rounded-tr-sm bg-black dark:bg-white' />
+        <div className='h-5 w-6 flex-shrink-0 rounded-bl-sm rounded-br-lg rounded-tl-lg rounded-tr-sm bg-cyan-800 dark:bg-white' />
       </Link>
     </>
   )
