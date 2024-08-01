15acaeb1396959bf8c836c5c354f5114fa3c9029
FredySetiawan <113951583+FredySetiawan@users.noreply.github.com>
2024-08-01 23:56:13 +0800

    [DEV] Style/skeletoncard (#31)
    * style(skeletoncard): Edit context card
    
    * style(skeletoncard): Edit context card

--- components/ui/skeleton/SkeletonCard.tsx
+++ components/ui/skeleton/SkeletonCard.tsx
@@ -2,12 +2,14 @@ export const SkeletonOne = () => {
   return (
     <div>
       <p className='text-xl font-bold text-white md:text-4xl'>
-        House in the woods
+        Tegenungan Waterfall
       </p>
       <p className='text-base font-normal text-white'></p>
       <p className='my-4 max-w-lg text-base font-normal text-neutral-200'>
-        A serene and tranquil retreat, this house in the woods offers a peaceful
-        escape from the hustle and bustle of city life.
+        Tegenungan Waterfall is a famous waterfall in Bali, located in
+        Tegenungan village, Kemenuh, Gianyar. With a height of about 15 meters,
+        this waterfall is surrounded by green trees and has a natural pool that
+        is suitable for swimming.
       </p>
     </div>
   )
@@ -16,14 +18,12 @@ export const SkeletonOne = () => {
 export const SkeletonTwo = () => {
   return (
     <div>
-      <p className='text-xl font-bold text-white md:text-4xl'>
-        House above the clouds
-      </p>
+      <p className='text-xl font-bold text-white md:text-4xl'>Monkey Forest</p>
       <p className='text-base font-normal text-white'></p>
       <p className='my-4 max-w-lg text-base font-normal text-neutral-200'>
-        Perched high above the world, this house offers breathtaking views and a
-        unique living experience. It&apos;s a place where the sky meets home,
-        and tranquility is a way of life.
+        Monkey Forest, or Mandala Suci Wenara Wana, is a nature reserve and
+        Hindu temple complex in Ubud, Bali, inhabited by around 900 Balinese
+        long-tailed monkeys.
       </p>
     </div>
   )
@@ -31,13 +31,14 @@ export const SkeletonTwo = () => {
 export const SkeletonThree = () => {
   return (
     <div>
-      <p className='text-xl font-bold text-white md:text-4xl'>
-        Greens all over
-      </p>
+      <p className='text-xl font-bold text-white md:text-4xl'>Jatiluwih</p>
       <p className='text-base font-normal text-white'></p>
       <p className='my-4 max-w-lg text-base font-normal text-neutral-200'>
-        A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
+        Jatiluwih is a terraced rice field area in Tabanan, Bali, renowned for
+        its natural beauty and a UNESCO World Heritage site. Situated at thefoot
+        of Mount Batukaru, this area of over 600 hectares showcases the
+        traditional Subak irrigation system that has been passed down through
+        generations.
       </p>
     </div>
   )
@@ -45,13 +46,14 @@ export const SkeletonThree = () => {
 export const SkeletonFour = () => {
   return (
     <div>
-      <p className='text-xl font-bold text-white md:text-4xl'>
-        Rivers are serene
-      </p>
+      <p className='text-xl font-bold text-white md:text-4xl'>Tirta Empul</p>
       <p className='text-base font-normal text-white'></p>
       <p className='my-4 max-w-lg text-base font-normal text-neutral-200'>
-        A house by the river is a place of peace and tranquility. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
+        Tirta Empul is a Hindu holy water temple located in the village of
+        Manukaya, near Tampaksiring, Bali. Founded in the 10th century, the
+        temple is famous for its holy springs used in the Melukat purification
+        ritual. The springs are believed to have been created by the god Indra
+        and are believed to have the power to purify those who bathe in them.
       </p>
     </div>
   )
@@ -59,13 +61,13 @@ export const SkeletonFour = () => {
 export const SkeletonFive = () => {
   return (
     <div>
-      <p className='text-xl font-bold text-white md:text-4xl'>
-        Rivers are serene
-      </p>
+      <p className='text-xl font-bold text-white md:text-4xl'>Mount Batur</p>
       <p className='text-base font-normal text-white'></p>
       <p className='my-4 max-w-lg text-base font-normal text-neutral-200'>
-        A house by the river is a place of peace and tranquility. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
+        Mount Batur is an active volcano in Kintamani, Bali, with a height of
+        about 1,717 meters. Famous for its stunning sunrise views, the climb
+        usually starts early in the morning to enjoy the moment from the summit,
+        providing an unforgettable experience.
       </p>
     </div>
   )
@@ -74,13 +76,15 @@ export const SkeletonFive = () => {
 export const SkeletonSix = () => {
   return (
     <div>
-      <p className='text-xl font-bold text-white md:text-4xl'>
-        Rivers are serene
-      </p>
+      <p className='text-xl font-bold text-white md:text-4xl'>Tanah Lot</p>
       <p className='text-base font-normal text-white'></p>
       <p className='my-4 max-w-lg text-base font-normal text-neutral-200'>
-        A house by the river is a place of peace and tranquility. It&apos;s the
-        perfect place to relax, unwind, and enjoy life.
+        Tanah Lot is a rock formation off the coast of Bali, home to an ancient
+        Hindu temple, Pura Tanah Lot. Located in Beraban, Kediri, Tabanan, the
+        temple is a famous cultural and tourist icon. The name &quot;Tanah
+        Lot&quot; comes from the words &quot;Tanah&quot; meaning island, and
+        &quot;Lot&quot; meaning sea, reflecting its unique location on a rock in
+        the middle of the sea.
       </p>
     </div>
   )
