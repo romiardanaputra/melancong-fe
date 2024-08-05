e7ef4a3d1902868403cf03efbe4e820b11021d44
romiardanaputra <romiardana21@gmail.com>
2024-08-05 15:56:28 +0800

    feat(data): Add data for profile navigation link

---
+++ app/(route)/dashboard/profile/index.data.tsx
@@ -0,0 +1,11 @@
+import {
+  IconBrandHipchat,
+  IconLockSquare,
+  IconSettings
+} from '@tabler/icons-react'
+
+export const ProfileLinkItems = [
+  { text: 'Terms and Conditions', icon: <IconSettings /> },
+  { text: 'Privacy Policy', icon: <IconLockSquare /> },
+  { text: 'Need Help? Let`s chat', icon: <IconBrandHipchat /> }
+]
