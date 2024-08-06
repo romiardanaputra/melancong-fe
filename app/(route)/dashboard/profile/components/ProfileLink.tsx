a5561d83fe3b041a9ce6534f67a07fc8315b1160
romiardanaputra <romiardana21@gmail.com>
2024-08-06 22:27:26 +0800

    chore: Update support component

--- app/(route)/dashboard/profile/components/ProfileLink.tsx
+++ app/(route)/dashboard/profile/components/ProfileLink.tsx
@@ -1,5 +1,5 @@
 import { IconChevronRight } from '@tabler/icons-react'
-import { ProfileLinkItems } from '../index.data'
+import { supportItems } from '../index.data'
 
 const ProfileLink = () => {
   return (
@@ -7,16 +7,21 @@ const ProfileLink = () => {
       <div className='my-4 space-y-4'>
         <h2 className='mb-3 pt-3 text-lg font-bold'>Support</h2>
         <ul className='list-none space-y-4 rounded-lg bg-white p-0 text-sm'>
-          {ProfileLinkItems.map(item => (
+          {supportItems.map(item => (
             <li
               key={item.text}
               className='flex cursor-pointer items-center border-b border-gray-300 p-3'
             >
-              <div className='flex items-center'>
+              <a
+                href={item.link}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='flex w-full items-center'
+              >
                 <span className='mr-3'>{item.icon}</span>
                 {item.text}
-              </div>
-              <IconChevronRight className='ml-auto text-gray-400' />
+                <IconChevronRight className='ml-auto text-gray-400' />
+              </a>
             </li>
           ))}
         </ul>
--- app/(route)/dashboard/profile/index.data.tsx
+++ app/(route)/dashboard/profile/index.data.tsx
@@ -4,8 +4,12 @@ import {
   IconSettings
 } from '@tabler/icons-react'
 
-export const ProfileLinkItems = [
-  { text: 'Terms and Conditions', icon: <IconSettings /> },
-  { text: 'Privacy Policy', icon: <IconLockSquare /> },
-  { text: 'Need Help? Let`s chat', icon: <IconBrandHipchat /> }
+export const supportItems = [
+  { text: 'Terms and Conditions', icon: <IconSettings />, link: '#' },
+  { text: 'Privacy Policy', icon: <IconLockSquare />, link: '#' },
+  {
+    text: 'Need Help? Let`s chat',
+    icon: <IconBrandHipchat />,
+    link: 'https://wa.me/62881037217902?text=Hello Admin, i`m looking for some help regarding your services. Could you please assist me?'
+  }
 ]
