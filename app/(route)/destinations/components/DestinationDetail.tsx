7644d24058524f0151ac1c223531f9914d0bc2c0
romiardanaputra <romiardana21@gmail.com>
2024-08-07 11:45:58 +0800

    style(destination detail): Fix page responsive on laptop and above

--- app/(route)/destinations/components/DestinationDetail.tsx
+++ app/(route)/destinations/components/DestinationDetail.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import { NextPage } from 'next'
-import Image from 'next/image'
+import Link from 'next/link'
 
 import withAuth from '@/app/withAuth'
-import { IconArrowLeft, IconLocation, IconStar } from '@tabler/icons-react'
+import { IconArrowLeft, IconMapPin, IconStar } from '@tabler/icons-react'
 import type { DestinationDetail, DetailProps } from '../index.props'
 import Loading from '../loading'
 import ErrorDestination from '../error'
@@ -21,45 +21,55 @@ const DestinationDetail: NextPage<DetailProps> = ({ id }) => {
   }
   return (
     <>
-      <div className='mx-auto max-w-md p-5 md:max-w-4xl'>
-        <div className='relative overflow-hidden rounded-lg bg-white shadow-md'>
-          <Image
-            src={destination?.imageLink || ''}
-            alt={destination?.name || ''}
-            className='h-64 w-full rounded-t-lg object-cover md:h-96'
-            layout='responsive'
-            loading='lazy'
-            placeholder='blur'
-            blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
-            width={500}
-            height={500}
-            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-          />
+      <div className='container mx-auto min-h-dvh max-w-md p-5 sm:max-w-4xl xl:max-w-full'>
+        <div className='relative grid grid-cols-1 overflow-hidden rounded-lg bg-white xl:grid-cols-2 xl:gap-8 xl:p-8'>
+          <div
+            className='h-60 rounded-md bg-cover bg-center bg-no-repeat sm:h-96 xl:h-full'
+            style={{ backgroundImage: `url(${destination?.imageLink || ''})` }}
+          ></div>
+
           <button
             onClick={() => window.history.back()}
             className='absolute left-2 top-2 rounded-full bg-white bg-opacity-70 p-2'
           >
             <IconArrowLeft />
           </button>
-          <div className='p-5'>
+          <div className='space-y-6 p-5'>
             <div className='mb-4 flex items-center justify-between'>
               <h1 className='text-2xl font-bold'>{destination?.name}</h1>
-              <span className='flex items-center rounded-full bg-green-100 px-2 py-1 text-sm text-green-800'>
-                <IconStar className='mr-1' /> {destination?.rating}
+              <span className='flex items-center rounded-full bg-green-100 px-2.5 py-2 text-sm text-green-800'>
+                <IconStar className='mr-1 size-4' /> {destination?.rating}
               </span>
             </div>
-            <div className='mb-4 flex items-center text-gray-600'>
-              <IconLocation /> {destination?.regency}, Bali
+            <div className='flex flex-col gap-2 text-sm text-gray-600 sm:flex-row sm:items-center'>
+              {destination?.location && (
+                <div className='flex w-fit items-center gap-2 rounded-full bg-cyan-700 px-4 py-2 text-neutral-200'>
+                  <IconMapPin className='size-5' />
+                  <Link href={destination?.location}>Google Maps</Link>
+                </div>
+              )}
             </div>
-            <div className='mb-4 flex items-center'>
-              <span className='mr-2 rounded-full bg-blue-100 px-2 py-1 text-blue-800'>
-                {destination?.childEntry}
-              </span>
-              <span className='rounded-full bg-blue-100 px-2 py-1 text-blue-800'>
-                {destination?.adultsEntry}
-              </span>
+            <div className='space-y-2 text-sm'>
+              <h3 className='font-bold text-neutral-700'>Location:</h3>
+              <div className='order-2 mb-2 flex items-center gap-2 sm:mb-0'>
+                <span>{destination?.regency}, Bali</span>
+              </div>
             </div>
-            <div className='mb-4 text-justify text-gray-700'>
+
+            <div className='space-y-4'>
+              <h3 className='text-sm font-bold text-neutral-700'>Pricing:</h3>
+              <div className='flex items-center text-sm font-medium'>
+                <span className='mr-2 rounded-full bg-blue-100 px-4 py-2 text-blue-800'>
+                  Child: {destination?.childEntry}
+                </span>
+                <span className='rounded-full bg-blue-100 px-4 py-2 text-blue-800'>
+                  Adult: {destination?.adultsEntry}
+                </span>
+              </div>
+            </div>
+
+            <div className='space-y-4 text-justify text-sm leading-relaxed text-gray-700'>
+              <span className='font-bold'>Description:</span>
               <div
                 dangerouslySetInnerHTML={{
                   __html: destination?.information || ''
