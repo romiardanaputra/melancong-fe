7598b6901324c83764bdbb248ccb9926f5d1ef7c
romiardanaputra <romiardana21@gmail.com>
2024-10-14 16:23:15 +0800

    refactor: Refine seo sitemap adding priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,43 +4,53 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: `${process.env.BASE_APP_URL}/`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 1
     },
     {
       url: `${process.env.BASE_APP_URL}/login`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/sign-up`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/dashboard`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/dashboard/saved`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/dashboard/profile`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/dashboard/profile/edit`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/dashboard/chatbot`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/landing`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     },
     {
       url: `${process.env.BASE_APP_URL}/dashboard/destinations`,
-      lastModified: new Date().toISOString()
+      lastModified: new Date().toISOString(),
+      priority: 0.8
     }
   ]
 }
